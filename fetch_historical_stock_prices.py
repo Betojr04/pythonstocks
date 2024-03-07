d1818a289a18e8de3134df7d7f389191d80764db
Betojr04 <betomasia12@gmail.com>
2024-03-07 13:18:52 -0700

    started adding the fetch for historical stock prices just placing the api key and url from alpha vantage

--- fetch_historical_stock_prices.py
+++ fetch_historical_stock_prices.py
@@ -3,3 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def fetch_historical_stock_prices(symbol):
+    api_key = os.environ.get('ALPHA_VANTAGE_API_KEY')
+    url = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey={api_key}')
+    
+    response = requests.get(url)
+    print(response.status_code)
