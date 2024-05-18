56bddc64e176b49d2d11ff93f49c927d0a23474b
Betojr04 <betomasia12@gmail.com>
2024-05-18 15:47:44 -0700

    initialized flask: installed dependancies flask, migrate, swagger, requests, sqlalchemy, jwt-extended, cors setup the flask project to start in app.py with api folder holding configuration with sqlite and init holding the function that starts the flask server

---
+++ server/api/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_object('api.config.Config')
+    
+    app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    CORS(app)
+    jwt.init_app(app)
+    
+    # from . import api_routes
+    # from . import auth
+    # app.register_blueprint(auth.auth)    
+    # app.register_blueprint(api_routes.api)
+    
+    
+    return app
--- server/main.py
+++ server/main.py
@@ -1,27 +1,27 @@
-from api.stock_fetcher import fetch_current_ticker_price
+# from server.api.utils.stock_fetcher import fetch_current_ticker_price
 
 
-def main():
+# def main():
     
-    while True:
-        ticker_symbol = input("Enter a ticker symbol: ")
+#     while True:
+#         ticker_symbol = input("Enter a ticker symbol: ")
 
-        price = fetch_current_ticker_price(ticker_symbol)
+#         price = fetch_current_ticker_price(ticker_symbol)
         
-        if price is not None:
-            print(f"The current price of {ticker_symbol} is ${price:.2f}")
-        else:
-            print(f"Failed to fetch the current price for {ticker_symbol}.")
+#         if price is not None:
+#             print(f"The current price of {ticker_symbol} is ${price:.2f}")
+#         else:
+#             print(f"Failed to fetch the current price for {ticker_symbol}.")
 
-        getting_more_ticker_prices = input("Would you like to see more ticker prices? (yes/no): ")
+#         getting_more_ticker_prices = input("Would you like to see more ticker prices? (yes/no): ")
         
-        if getting_more_ticker_prices.lower() != "yes":
-            print("Okay, Goodbye!")
-            break
+#         if getting_more_ticker_prices.lower() != "yes":
+#             print("Okay, Goodbye!")
+#             break
             
             
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
     
     
     
