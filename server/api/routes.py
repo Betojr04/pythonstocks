de97ca5005d23798a59490832f5e2265c18bfa31
Betojr04 <betomasia12@gmail.com>
2024-05-19 10:37:34 -0700

    added a route to view historical stock info, prices etc. at default time period of a year and at default interval of a day at a time

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
-import requests
 import yfinance as yf
 
 api = Blueprint("api", __name__)
 
+
+# ROUTE TO RETRIEVE MOST CURRENT STOCK PRICE
 @api.route("/current_price", methods=["GET"])
 def fetch_current_ticker_price():
     ticker_symbol = request.args.get('ticker')
@@ -17,3 +18,30 @@ def fetch_current_ticker_price():
     except Exception as e:
         print(f"Error fetching data for {ticker_symbol}: {e}")
         return jsonify({"error": "Unable to fetch data"}), 500
+    
+    
+# ROUTE TO RETREIVE HISTORICAL STOCK INFORMATION IN INTERVALS
+@api.route("/historical_stock_info", methods=["GET"])
+def historical_stock_prices():
+    ticker_symbol = request.args.get('ticker')
+    period = request.args.get('period', '1y')
+    interval = request.args.get('interval', '1d')
+    
+    if not ticker_symbol:
+        return jsonify({"error" : "Ticker Symbol is required"}), 400
+    
+    try:
+        ticker = yf.Ticker(ticker_symbol)
+        hist = ticker.history(period=period, interval=interval)
+        
+        if hist.empty:
+            print(f"No data found for ticker: {ticker_symbol}")
+            return jsonify({"error": "No data found for the given parameters"}), 404
+
+        data = hist.reset_index().to_dict(orient='records')
+        print(f"Data to be returned: {data}")
+        return jsonify(data)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        return jsonify({"error": str(e)}), 500
+    
